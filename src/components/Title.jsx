cc3c2c451cfe86ecbc311d6026d964cc3b1247cd
Xavierd <xavier.desoindre@hotmail.fr>
2021-02-16 09:45:27 +0100

    rajout du titre

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Title extends React.Component {
+    render() {
+        return (
+            <h1>Git kraken c'est cool</h1>
+        )
+    }
+}
+
+export default Title;
